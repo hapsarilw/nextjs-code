1c2f4b6bf634700be96a1df4ddc17fa577819123
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2022-03-31 20:54:57 +0800

    Navigating with the Link Component

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,9 @@
-function ClientPage(){
-    return <div>
-        <h1>Client Page!</h1>
+function ClientPage() {
+  return (
+    <div>
+      <h1>Client Page!</h1>
     </div>
+  );
 }
 
 export default ClientPage;
