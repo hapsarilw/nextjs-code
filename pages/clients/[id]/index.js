d9cd88f690eb21abbf51f321ecb95f9f2c9fe8ef
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2022-04-03 16:08:30 +0800

    navigating programatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,19 @@ function CientProjectsPage() {
   const router = useRouter();
   /* access to concrete id value */
   console.log(router.query);
+
+  function loadProjectHandler(){
+    // load data...
+    router.push({
+      pathname: 'clienrs/[id]/[clientprojectid]',
+      query: { id: '1', clientprojectid: 'projecta' }
+    })
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client!</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
