8db0baec9b1cd3ecd90af02402b47e6d780dd9c1
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2022-04-07 00:07:56 +0800

    Adding general layout wrapper component & all event pages

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,31 +1,46 @@
 import Link from "next/link";
 
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+import classes from "./event-item.module.css";
+
 function EventItem(props) {
-    const {title, image, date, location, id} = props;
+  const { title, image, date, location, id } = props;
 
-    const humanReadableDate = new Date(date).toLocaleDateString('en-US',{
-        day: 'numeric',
-        month: 'long',
-        year: "numeric", 
-    });
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 
-    const formattedAddress = location.replace(', ', '\n');
-    const exploreLink = `/events/${id}`;
+  const formattedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
 
   return (
-    <li>
-      <img src={'/' + image} alt={title} />
-      <div>
-        <h2>{title}</h2>
-        <div>
-          <time>{humanReadableDate}</time>
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
+          </div>
         </div>
-        <div>
-          <address>{formattedAddress}</address>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
-      </div>
-      <div>
-        <Link href={exploreLink}>Explore Event</Link>
       </div>
     </li>
   );
