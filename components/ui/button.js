93a45f425d77d2a701cb7c85f1051958db0739cc
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2022-04-07 06:34:47 +0800

    Adding a Filter Form for Filtering Events

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,11 +2,20 @@ import Link from "next/link";
 import classes from "./button.module.css";
 
 function Button(props) {
-  return (
-    <Link href={props.link}>
-      <a className={classes.btn}>{props.children}</a>
-    </Link>
-  ); 
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  } else {
+    return (
+      // onClick -> use our own button just as the built in button
+      <button className={classes.btn} onClick={props.click}>
+        {props.children}
+      </button>
+    );
+  }
 }
 
 export default Button;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,18 @@
 import { getAllEvents } from '../../dummy-data';
+import { Fragment } from 'react/cjs/react.production.min';
 import EventList from '../../components/events/event-list';
+import EventSearch from '../../components/events/event-search';
+
 
 
 function AllEventsPage(){
-    const events = getAllEvents();
+    const events = getAllEvents(); 
+    
     return (
-        <div>
+        <Fragment>
+            <EventSearch/>
             <EventList items={events}/>
-        </div>
+        </Fragment>
     );
 
 }
