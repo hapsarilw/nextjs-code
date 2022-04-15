453ce1853f7c7cf17494b176688f39e5bfd671d9
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2022-04-15 19:57:20 +0800

    sending requests to api routes

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 
 function HomePage() {
   const emailInputRef = useRef();
   const feedbackInputRef = useRef();
 
-  function submitForwardHandler(event){
+  function submitFormHandler(event) {
     event.preventDefault();
-    
+
     const enteredEmail = emailInputRef.current.value;
     const enteredFeedback = feedbackInputRef.current.value;
-    
+
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   return (
@@ -18,11 +29,11 @@ function HomePage() {
       <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" ref={emailInputRef}/>
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Your Feedback</label>
-          <textarea id='feedback' rows='5' ref={feedbackInputRef}></textarea>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
         </div>
         <button>Send Feedback</button>
       </form>
