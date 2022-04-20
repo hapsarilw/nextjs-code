3253eacfe27d098fc48bccff1bd0e7f69ae15ceb
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2022-04-20 11:39:45 +0800

    adding all post and function to read & fetch data from markdown files

---
+++ pages/posts/[slug].js
@@ -0,0 +1,8 @@
+import PostContent from "../../components/posts/post-detail/post-content";
+
+// /posts/getting-started-with-next-js -> slug identifier & human readable
+function PostDetailPage() {
+    return <PostContent/>
+}
+
+export default PostDetailPage;
