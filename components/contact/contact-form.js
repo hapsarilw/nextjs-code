e1366877f2822a755a5f7216a338de4d7058cff9
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2022-04-24 14:30:04 +0800

    storing messages with mongoDB in database

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,28 +1,73 @@
-import classes from './contact-form.module.css';
+import { useState } from "react";
+import classes from "./contact-form.module.css";
 
-function ContactForm (){
-    return <section className={classes.contact}>
-        <h2>How can I help you?</h2>
-        <form className={classes.form}>
-            <div className={classes.controls}>
-                <div className={classes.control}>
-                    <label htmlFor="email">Your Email</label>
-                    <input type='email' id='email required'/>
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor="name">Your Name</label>
-                    <input type='text' id='email' required/>
-                </div>
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="message">Your Message</label>
-                <textarea id="message" rows="5"></textarea>
-            </div>
-            <div className={classes.actions}>
-                <button>Send Message</button>
-            </div>
-        </form>
+function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    // optional: add client-side validation
+
+    // Wanna send http request to API route
+
+    fetch("/api/contact/", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  return (
+    <section className={classes.contact}>
+      <h2>How can I help you?</h2>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(event) => setEnteredEmail(event.target.value)}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="name">Your Name</label>
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(event) => setEnteredName(event.target.value)}
+            />
+          </div>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="message">Your Message</label>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            value={enteredMessage}
+            onChange={(event) => setEnteredMessage(event.target.value)}
+          ></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Send Message</button>
+        </div>
+      </form>
     </section>
+  );
 }
 
 export default ContactForm;
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,24 +1,59 @@
-function handler(req, res){
-    if(req.method === 'POST'){
-        // nextJs will parse the body of the request
-        const { email, name, message } = req.body;
+import { MongoClient } from "mongodb";
 
-        if(!email || !email.include('@') || !name || name.trim() === '' || !message || message.trim() ===''){
-            res.status(422).json({ message: 'Invalid input.'})
-            return;
-        }
+async function handler(req, res) {
+  if (req.method === "POST") {
+    // nextJs will parse the body of the request
+    const { email, name, message } = req.body;
 
-        // Store it in a database
-        const newMessage = {
-            email,
-            name, 
-            message
-        };
-
-        console.log(newMessage);
-
-        res.status(201).json({message: 'Successfully stored message!', message: newMessage})
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
     }
+
+    // Store it in a database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    console.log(newMessage);
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://amihapsari:z8rNVZeg0PaayJGW@cluster0.wq6iu.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage._id = result.insertedId; 
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed" });
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: "Successfully stored message!", message: newMessage });
+  }
 }
 
 export default handler;
